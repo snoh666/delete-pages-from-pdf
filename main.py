300abb136fb53a3e2f079da399ed2031335972f9
Snochowski Mikołaj <snochopl@gmail.com>
2020-10-01 14:44:14 +0200

    Created input functions to get proper user input names for files

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
+filename_to_open = input('Please input filename to open with *.pdf extension: ')
+
+name_to_save = input('Input name for file to save as (with *.pdf extension): ')
+
 pages_to_keep = [ 5 ] # page numbering starts from 0
-infile = PdfFileReader('document.pdf', 'rb')
+infile = PdfFileReader(filename_to_open, 'rb')
 output = PdfFileWriter()
 
 for i in pages_to_keep:
     p = infile.getPage(i)
     output.addPage(p)
 
-with open('newfile1.pdf', 'wb') as f:
+with open(name_to_save, 'wb') as f:
     output.write(f)
